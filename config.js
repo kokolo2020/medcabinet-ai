33b64761c06f111639e3e734f6e903b0b54b761e
kokolo2020 <66770576+kokolo2020@users.noreply.github.com>
2026-07-18 15:41:07 +0700

    Fix broken config and restore app loading

--- config.js
+++ config.js
@@ -2,10 +2,3 @@ window.MEDCABINET_CONFIG = {
   supabaseUrl: 'https://cdjkpwvxlrtnsftfbiew.supabase.co',
   supabaseKey: 'sb_publishable_AI_74SXuCbwYQG5T4H5OUA_VmfDxVbd'
 };
-
-(function loadPremiumCabinetStyles(){
-  if(document.querySelector('link[data-premium-cabinet]')) return;
-  var link=document.createElement('link');
-  link.rel='stylesheet';
-  link.href='premium-cabinet.css?v=3';
-  link.setAttribute('data-prem
